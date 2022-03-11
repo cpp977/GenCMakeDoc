aea9ab21587307265bf7012fe3945b9b03943c2f
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-03-11 10:06:26 +0100

    Renamed two parameters and made sure that the script exits if the cmake command fails.

--- make_doc.py
+++ make_doc.py
@@ -8,8 +8,8 @@ parser = argparse.ArgumentParser(description='Generate documentation for cmake b
 parser.add_argument('--source-dir', type=str, default='.', dest='source_dir', help='Where the CMakeLists.txt is.')
 parser.add_argument('--filter', type=str, default='', dest='opt_filter', help='Specify a filter for cmake options to process. For getting options like MYLIB_SOME_OPT, set filter to MYLIB. If empty, no filter is applied.')
 parser.add_argument('--outfile', type=str, default='cmake_doc.md', dest='outfile', help='Filename of the output.')
-parser.add_argument('--section-title', type=str, default='Build Options', dest='section_title', help='Title of the section in the README file where the build options are in.')
-parser.add_argument('--section-prefix', type=str, default='##', dest='section_prefix', help='Markdown specifier for the section. Normally ##')
+parser.add_argument('--heading-title', type=str, default='Build Options', dest='heading_title', help='Title of the section in the README file where the build options are in.')
+parser.add_argument('--heading-marker', type=str, default='#', dest='heading_marker', help='Markdown specifier for the heading. Normally #')
 parser.add_argument('--README-name', type=str, default='README.md', dest='README_name', help='Name of the README file relative to source-dir')
 
 args = parser.parse_args()
@@ -30,7 +30,7 @@ class CMakeOption:
         return f"Option {self.name} with default={self.default}. Description={self.description}. Deduced category={self.category}."
     
 with tempfile.TemporaryDirectory() as tmpdirname:
-    process = subprocess.run(['cmake', '-LH', args.source_dir], capture_output=True, text=True, cwd=tmpdirname)
+    process = subprocess.run(['cmake', '-LH', args.source_dir], capture_output=True, text=True, cwd=tmpdirname, check=True)
     filtered = process.stdout[process.stdout.find("// "):]
     plain_options = filtered.split("\n\n")
     options = []
@@ -58,12 +58,10 @@ with tempfile.TemporaryDirectory() as tmpdirname:
      
     with open(args.source_dir+"/"+args.README_name, 'r+') as md_file:
         data = md_file.read()
-        begin = data.find(args.section_prefix + " " + args.section_title) + len(args.section_prefix + " " + args.section_title)
-        end = begin+data[begin:].find(args.section_prefix)
+        begin = data.find(args.heading_marker + " " + args.heading_title) + len(args.heading_marker + " " + args.heading_title)
+        end = begin+data[begin:].find("# ")
         if end < begin:
             end = len(data)
-        else:
-            print(data[end:])
         data = data[:begin] + strTable + "\n" + data[end:]
         md_file.truncate(0)
         md_file.seek(0)
